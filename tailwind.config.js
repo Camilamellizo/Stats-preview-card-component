bd4bf6afc448a493bfe52ccd38718e53f797a4c1
camila <112856775+Camilamellizo@users.noreply.github.com>
2024-01-10 14:32:33 +0100

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    content: ["./*.{html,js}"],
+  ],
   theme: {
     extend: {},
   },
